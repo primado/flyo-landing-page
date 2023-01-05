d18cf2efa01705aa26b95e1cb3c97e7ec323a791
primado <primadoprincepa@gmail.com>
2023-01-05 14:25:03 +0000

    added animation bounce & extra styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,8 +57,19 @@ module.exports = {
       // => @media (min-width: 1536px) { ... }
     },
 
+   // animation bounce in tailwind css in 5ms
+    animation: {
+      'bounce': 'bounce 4s infinite',
+    },
+    keyframes: {
+      bounce: {
+        '0%, 100%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.8,0,1,1)' },
+        '50%': { transform: 'translateY(0)', animationTimingFunction: 'cubic-bezier(0,0,0.2,1)' },
+      },
+    },
 
     extend: {},
+
   },
 
 
